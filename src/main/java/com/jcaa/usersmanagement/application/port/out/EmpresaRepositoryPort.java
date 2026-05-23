61d876676e83627eb56e295997228b1e4fc52d10
rreyesj-afk <rreyesj@unicartagena.edu.co>
2026-05-23 13:32:31 -0500

    refactor(ports-out): change parameters name for EmpresaRepository and add find empresa by name

--- src/main/java/com/jcaa/usersmanagement/application/port/out/EmpresaRepositoryPort.java
+++ src/main/java/com/jcaa/usersmanagement/application/port/out/EmpresaRepositoryPort.java
@@ -1,7 +1,10 @@
 package com.jcaa.usersmanagement.application.port.out;
 
 import com.jcaa.usersmanagement.domain.model.EmpresaModel;
+import com.jcaa.usersmanagement.domain.model.UserModel;
 import com.jcaa.usersmanagement.domain.valueobject.EmpresaId;
+import com.jcaa.usersmanagement.domain.valueobject.EmpresaName;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -12,14 +15,17 @@ public interface EmpresaRepositoryPort {
     EmpresaModel save(EmpresaModel empresa);
 
     //encontrar por id
-    Optional<EmpresaModel> findById(EmpresaId empresaId);
+    Optional<EmpresaModel> findById(EmpresaId idEmpresa);
+
+    //encontrar por nombre
+    Optional<UserModel> findByName(EmpresaName nameEmpresa);
 
     //mostrar todas las id
     List<EmpresaModel> findAll();
 
     //id existente
-    boolean existsById(EmpresaId empresaId);
+    boolean existsById(EmpresaId idEmpresa);
 
     //eliminar empresa
-    void delete(EmpresaId empresaId);
+    void delete(EmpresaId idEmpresa);
 }
