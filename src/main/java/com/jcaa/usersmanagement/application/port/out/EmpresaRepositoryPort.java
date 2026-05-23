994b4ed0d597a339e5cac30c866ee759f7b0682e
rreyesj-afk <rreyesj@unicartagena.edu.co>
2026-05-23 11:51:37 -0500

    feat(ports-in): add inbound ports for empresa operations

--- src/main/java/com/jcaa/usersmanagement/application/port/out/EmpresaRepositoryPort.java
+++ src/main/java/com/jcaa/usersmanagement/application/port/out/EmpresaRepositoryPort.java
@@ -10,12 +10,16 @@ public interface EmpresaRepositoryPort {
 
     //guardar empresa
     EmpresaModel save(EmpresaModel empresa);
+
     //encontrar por id
     Optional<EmpresaModel> findById(EmpresaId empresaId);
+
     //mostrar todas las id
     List<EmpresaModel> findAll();
+
     //id existente
     boolean existsById(EmpresaId empresaId);
+
     //eliminar empresa
     void delete(EmpresaId empresaId);
 }
