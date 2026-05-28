572a7652fddf2ee760459a6e4a2a69ac0e2039ed
rreyesj-afk <rreyesj@unicartagena.edu.co>
2026-05-28 01:49:40 -0500

    refactor(persistence): update PersistenceException to use specific operations

--- src/main/java/com/jcaa/usersmanagement/infrastructure/adapter/persistence/exception/PersistenceException.java
+++ src/main/java/com/jcaa/usersmanagement/infrastructure/adapter/persistence/exception/PersistenceException.java
@@ -8,14 +8,15 @@ public final class PersistenceException extends RuntimeException {
   private static final String MESSAGE_EMAIL = "Failed to find user with email: '%s'.";
   private static final String MESSAGE_ALL = "Failed to retrieve all users.";
   private static final String MESSAGE_DELETE = "Failed to delete user with ID: '%s'.";
-
   // EMPRESA
- private static final String MESSAGE_EMPRESA_SAVE = "Failed to save empresa with ID: '%s'.";
- private static final String MESSAGE_EMPRESA_UPDATE = "Failed to update empresa with ID: '%s'.";
- private static final String MESSAGE_EMPRESA_FIND = "Failed to find empresa with ID: '%s'.";
- private static final String MESSAGE_EMPRESA_NAME = "Failed to find empresa with name: '%s'.";
- private static final String MESSAGE_EMPRESA_ALL = "Failed to retrieve all empresas.";
- private static final String MESSAGE_EMPRESA_DELETE = "Failed to delete empresa with ID: '%s'.";
+  private static final String MESSAGE_EMPRESA_SAVE = "Failed to save empresa with ID: '%s'.";
+  private static final String MESSAGE_EMPRESA_UPDATE_ANNUAL_BILLING = "Failed to update annual billing of empresa with ID: '%s'.";
+  private static final String MESSAGE_EMPRESA_CHANGE_SEDE = "Failed to change sede of empresa with ID: '%s'.";
+  private static final String MESSAGE_EMPRESA_CHANGE_SECTOR = "Failed to change sector of empresa with ID: '%s'.";
+  private static final String MESSAGE_EMPRESA_FIND = "Failed to find empresa with ID: '%s'.";
+  private static final String MESSAGE_EMPRESA_NAME = "Failed to find empresa with name: '%s'.";
+  private static final String MESSAGE_EMPRESA_ALL = "Failed to retrieve all empresas.";
+  private static final String MESSAGE_EMPRESA_DELETE = "Failed to delete empresa with ID: '%s'.";
 
 
   private static final String MESSAGE_CONNECTION = "Could not establish database connection.";
@@ -64,20 +65,30 @@ public final class PersistenceException extends RuntimeException {
     return new PersistenceException(String.format(MESSAGE_EMPRESA_SAVE, idEmpresa), cause);
   }
 
-  public static PersistenceException becauseEmpresaUpdateFailed(final String idEmpresa, final Throwable cause) {
-   return new PersistenceException(String.format(MESSAGE_EMPRESA_UPDATE, idEmpresa), cause
-    );
-  }
+    public static PersistenceException becauseEmpresaUpdateAnnualBillingFailed(final String idEmpresa, final Throwable cause) {
+        return new PersistenceException(String.format(MESSAGE_EMPRESA_UPDATE_ANNUAL_BILLING, idEmpresa), cause
+        );
+    }
 
-  public static PersistenceException becauseEmpresaFindByIdFailed(final String idEmpresa, final Throwable cause) {
-    return new PersistenceException(String.format(MESSAGE_EMPRESA_FIND, idEmpresa), cause);
-  }
+    public static PersistenceException becauseEmpresaChangeSedeFailed(final String idEmpresa, final Throwable cause) {
+        return new PersistenceException(String.format(MESSAGE_EMPRESA_CHANGE_SEDE, idEmpresa), cause
+        );
+    }
 
-  public static PersistenceException becauseFindByNameFailed(final String empresaName, final Throwable cause) {
-    return new PersistenceException(String.format(MESSAGE_EMPRESA_NAME, empresaName), cause);
-  }
+    public static PersistenceException becauseEmpresaChangeSectorFailed(final String idEmpresa, final Throwable cause) {
+        return new PersistenceException(String.format(MESSAGE_EMPRESA_CHANGE_SECTOR, idEmpresa), cause
+        );
+    }
 
-  public static PersistenceException becauseEmpresaFindAllFailed(final Throwable cause) {
+    public static PersistenceException becauseFindEmpresaByIdFailed(final String idEmpresa, final Throwable cause) {
+        return new PersistenceException(String.format(MESSAGE_EMPRESA_FIND, idEmpresa), cause);
+    }
+
+    public static PersistenceException becauseFindEmpresaByNameFailed(final String empresaName, final Throwable cause) {
+        return new PersistenceException(String.format(MESSAGE_EMPRESA_NAME, empresaName), cause);
+    }
+
+  public static PersistenceException becauseFindAllEmpresasFailed(final Throwable cause) {
     return new PersistenceException(MESSAGE_EMPRESA_ALL, cause);
   }
 
